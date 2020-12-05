cc49d8f31ebc7b7b9fe2a9149da6fd7ada8924ab
Kevin Tom <kevinktom@gmail.com>
2020-12-05 13:46:16 -0500

    Refactored home component into a class

--- pick-me-ups/src/home/Home.tsx
+++ pick-me-ups/src/home/Home.tsx
@@ -1,31 +1,39 @@
+import { render } from "@testing-library/react";
 import React from "react";
 import { Link } from "react-router-dom";
 import './home.css';
 
-const Home: React.FC = () => {
-  return (
-    <>
-      <h1 id='header'>Pick me ups</h1>
-      <Link to="/quote"> back to quote page</Link>
+class Home extends React.Component{
 
-      <div id='cardBody'>
-          {moods.map(mood => (
-            <div id='moods'>
-              <p>{mood}</p>
-            </div>
-          ))}
-      </div>
-      &nbsp;
+  render() {
+    return (
+      <>
+        <h1 id='header'>Pick me ups</h1>
+        <Link to="/quote"> back to quote page</Link>
+  
+        <div id='cardBody'
+              >
+            {this.moods.map(mood => (
+              <div id='moods'>
+                <p>{mood}</p>
+              </div>
+            ))}
+        </div>
+        &nbsp;
+  
+      </>
+    );
+  };
 
-    </>
-  );
-};
+  moods = ["I'm feeling depressed",
+                 "I hope I don't mess up",
+                 "Everyone is going to laugh at me",
+                 "I'm not good enough",
+                 "What's the point?"  
+          ];
+}
 
-const moods = ["I'm feeling depressed",
-               "I hope I don't mess up",
-               "Everyone is going to laugh at me",
-               "I'm not good enough",
-               "What's the point?"  
-              ];
+
+
 
 export default Home;
